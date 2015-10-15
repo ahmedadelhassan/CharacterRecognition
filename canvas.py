13a6b882181bff5bd4463ab6dcffd4bb70e55090
ahmedadelhassan <ahmed.adel.hassan@hotmail.com>
2015-10-15 20:42:59 +0200

    Drawing, clearing and saving functions all working

--- canvas.py
+++ canvas.py
@@ -1,55 +1,88 @@
 #!/usr/bin/python
 '''
 Created on Oct 11, 2015
-
+A drawer GUI to obtain user input using mouse click and drag.
+It has Clear and Save to JPG options (saved image resolution fixed at 100*100 
+   pixels)
 @author: ahmad
 '''
 
-from Tkinter import *
+import sys
+from Tkinter import Tk, Canvas, Button
+from PIL import Image, ImageDraw
 
-"""paint.py: not exactly a paint program.. just a smooth line drawing demo."""
+# Define global variables
+drawing_area = ""
+x,y=None,None
+count=0
+image_cnt = 0
+image_name = "canvas_"
+# Initialize new PIL Image and a draw handler
+PIL_image = Image.new("1", (300, 300), "white")
+draw = ImageDraw.Draw(PIL_image)
 
-b1 = "up"
-xold, yold = None, None
+
+def quit(event):
+    sys.exit()
+   
+def clear(event):
+    global drawing_area, PIL_image, draw
+    drawing_area.delete("all")
+    PIL_image = Image.new("1", (300, 300), "white")
+    draw = ImageDraw.Draw(PIL_image)
+
+def drag(event):
+    global drawing_area, x,y, count, draw
+    newx,newy=event.x,event.y
+    if x is None:
+        x,y=newx,newy
+        return
+    count+=1
+    sys.stdout.write("\revent count %d"%count)
+    drawing_area.create_line((x,y,newx,newy), width=5, smooth=True)
+    draw.line((x,y,newx,newy), width=3)
+    x,y=newx,newy
+
+def drag_end(event):
+    global x,y
+    x,y=None,None
+   
+def save(event):
+    global PIL_image, image_name, image_cnt
+    image_cnt += 1
+    file_name = image_name + str(image_cnt) + ".jpg"
+    PIL_image = PIL_image.resize((100,100), Image.ANTIALIAS)
+    PIL_image.save(file_name)
+
 
 def main():
+    global drawing_area
+    
     root = Tk()
-    drawing_area = Canvas(root)
+    root.title("Drawer")
+    drawing_area=Canvas(root, width=300, height=300, bg="white")
+     
+    # Binding Events to the canvas
+    drawing_area.bind("<B1-Motion>",drag)
+    drawing_area.bind("<ButtonRelease-1>",drag_end)
+    drawing_area.pack()
     
     #Buttons
-    #Clear Buttons
-    clear_button = Button(root, width=10, height=2, command=clear(drawing_area), text="Clear")
-    clear_button.place(bordermode=INSIDE, x=0, y=0)
+    #Quit Button
+    b1=Button(root,text="Quit")
+    b1.pack()
+    b1.bind("<Button-1>",quit)
     
+    #Clear Button
+    b2=Button(root,text="Clear")
+    b2.pack()
+    b2.bind("<Button-1>",clear)
     
-    drawing_area.pack()
-    drawing_area.bind("<Motion>", motion)
-    drawing_area.bind("<ButtonPress-1>", b1down)
-    drawing_area.bind("<ButtonRelease-1>", b1up)
+    #Save Button
+    b3=Button(root,text="Save")
+    b3.pack()
+    b3.bind("<Button-1>",save)
     root.mainloop()
-
-def clear(canvas):
-    canvas.delete("all")
-
-def b1down(event):
-    global b1
-    b1 = "down"           # you only want to draw when the button is down
-                          # because "Motion" events happen -all the time-
-
-def b1up(event):
-    global b1, xold, yold
-    b1 = "up"
-    xold = None           # reset the line when you let go of the button
-    yold = None
-
-def motion(event):
-    if b1 == "down":
-        global xold, yold
-        if xold is not None and yold is not None:
-            event.widget.create_line(xold,yold,event.x,event.y,smooth=TRUE, fill = "blue", width=6)
-                          # here's where you draw it. smooth. neat.
-        xold = event.x
-        yold = event.y
 
 if __name__ == "__main__":
     main()
