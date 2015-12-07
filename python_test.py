c8e378da7196b4cfedf0c2d167d495f84b40da32
osm3000 <omar.samir3000@gmail.com>
2015-12-07 16:00:45 +0100

    This is a test file to make sure the dependencies are resolved

---
+++ python_test.py
@@ -0,0 +1,20 @@
+import numpy
+import matplotlib
+try:
+    from Tkinter import *
+except ImportError:
+    from tkinter import *
+try:
+    import ttk
+    py3 = 0
+except ImportError:
+    import tkinter.ttk
+    py3 = 1
+from tkFileDialog import *
+import PIL
+import cv2
+import skimage
+import seqlearn
+import sklearn
+import nltk
+import pyxdameraulevenshtein
